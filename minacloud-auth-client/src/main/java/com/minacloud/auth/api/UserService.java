207b68fcf0d44b552208184c835bf9ebdd956607
Laysan <lslvxy@gmail.com>
2021-09-16 11:32:42 +0800

    添加接口

--- minacloud-auth-client/src/main/java/com/minacloud/auth/api/UserService.java
+++ minacloud-auth-client/src/main/java/com/minacloud/auth/api/UserService.java
@@ -3,11 +3,13 @@ package com.minacloud.auth.api;
 import com.alibaba.cola.dto.Response;
 import com.minacloud.auth.dto.cmd.UserAddCmd;
 import com.minacloud.auth.dto.cmd.UserUpdateCmd;
+import com.minacloud.auth.dto.cmd.UserDeleteCmd;
 
 public interface UserService {
     Response addUser(UserAddCmd cmd);
 
     Response updateUser(UserUpdateCmd cmd);
 
+    Response deleteUser(UserDeleteCmd cmd);
 
 }
